6cebc5f81606eabc4d61d64221df24ffd045db7e
alissonmuniz17 <alissonmuniz17@gmail.com>
2025-05-27 16:06:17 -0300

    Update EKS.tf

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -1,8 +1,9 @@
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
   cluster_name                    = var.cluster_name
-  cluster_version                 = "~> 19.0"
+  cluster_version                 = "1.30"
   cluster_endpoint_public_access  = true
 
   cluster_creator_admin_permissions = true

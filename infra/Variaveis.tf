fb2eed4cd1cc9c7ed8f4cde5a62832ceae9b3910
alissonmuniz17 <alissonmuniz17@gmail.com>
2025-05-27 17:12:08 -0300

    Update Variaveis.tf

--- infra/Variaveis.tf
+++ infra/Variaveis.tf
@@ -1,3 +1,10 @@
 variable "cluster_name" {
   type = string
 }
+
+variable "cluster_creator_admin_permissions" {
+  description = "If true, grants cluster admin permissions to the creator"
+  type        = bool
+  default     = true
+}
+

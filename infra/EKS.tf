66ece55e3676739be289532c112fb5690427a293
alissonmuniz17 <alissonmuniz17@gmail.com>
2025-05-27 15:36:32 -0300

    Update EKS.tf

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -5,6 +5,8 @@ module "eks" {
   cluster_version                 = "1.30"
   cluster_endpoint_public_access  = true
 
+  cluster_creator_admin_permissions = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
 
